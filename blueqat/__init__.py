42bce5aae617419c50cfff194599f9612eccb9c0
gyu-don <takumi.kt+git@gmail.com>
2019-03-04 16:44:52 +0900

    quma-- :bear:

--- blueqat/__init__.py
+++ blueqat/__init__.py
@@ -3,5 +3,6 @@ from . import pauli
 from . import utils
 from . import vqe
 from . import opt
+from . import quma
 
-__all__ = ["pauli", "utils", "vqe", "opt", "Circuit", "BlueqatGlobalSetting"]
+__all__ = ["pauli", "utils", "vqe", "opt", "quma", "Circuit", "BlueqatGlobalSetting"]
